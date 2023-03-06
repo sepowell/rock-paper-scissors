c56bc4e8182f85598560271e382fb8d6e0d235cd
sepowell <seanp1998@gmail.com>
2023-03-06 12:22:22 -0800

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -20,5 +20,49 @@ function getComputerChoice() {
     case 2:
       return "Scissors";
   }
-  
 }
+
+let playerChoice = "Rock";
+let computerChoice = getComputerChoice(); 
+
+/**
+ * playRound function takes input from playerChoice
+ * and computerChoice variable, (computerChoice
+ * variable is assigned to getComputerChoice function,
+ * while playerChoice variable is static for now) and runs
+ * the inputs through a nested if else statement hooking 
+ * from the playersChoice first to find the outcome
+ * and returns the result of the round. 
+ */
+
+function playRound (playerChoice, computerChoice) {
+  if (playerChoice == "Rock") {
+    if (computerChoice == "Rock") {
+      return "Tie!";
+    } else if (computerChoice == "Paper") {
+      return "You lose! Paper beats Rock!";
+    } else if (computerChoice == "Scissors") {
+      return "You win! Rock beats Scissors!"
+    }
+
+  } else if (playerChoice == "Paper") {
+    if (computerChoice == "Rock") {
+      return "You win! Paper beats Rock!";
+    } else if (computerChoice == "Paper") {
+      return "Tie!";
+    } else if (computerChoice == "Scissors") {
+      return "You Lose! Scissors beats Paper!"
+    }
+
+  } else if (playerChoice == "Scissors") {
+    if (computerChoice == "Rock") {
+      return "You Lose! Rock beats Scissors!";
+    } else if (computerChoice == "Paper") {
+      return "You win! Scissors beats Paper!";
+    } else if (computerChoice == "Scissors") {
+      return "Tie!"
+    }
+  }
+}   
+
+console.log(playRound(playerChoice, computerChoice));
