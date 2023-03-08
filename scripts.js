5c700dadf0db9df68497914d5bf95714aa81a2ec
sepowell <seanp1998@gmail.com>
2023-03-07 18:31:51 -0800

    Finish basic structure of game

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 
-let playerChoice = getPlayerChoice();
+let playerChoice = "";
 let computerChoice = getComputerChoice(); 
+
+let playerScore = document.getElementById("playerScore");
+let computerScore = document.getElementById("computerScore");
+let playerWins = 0;
+let computerWins = 0;
 
 let gameResult = document.getElementById("gameResult");
 let roundResult = document.getElementById("roundResult");
@@ -8,24 +13,27 @@ let gameScore = document.getElementById("gameScore");
 
 /**
  * Function getPlayerChoice recieves input from user 
- * then for now just logs it out. 
+ * then triggers the game function.
  */
 
 function getPlayerChoice(choice) {
   switch (choice) {
     case 'Rock':
-      console.log("Rock");
-      break;
-    
-    case 'Paper':
-      console.log("Paper");
-      break;
-
-    case 'Scissors':
-      console.log("Scissors");
+      playerChoice = "Rock";
       break;
       
+    case 'Paper':
+      playerChoice = "Paper";
+      break;
+      
+    case 'Scissors':
+      playerChoice = "Scissors";
+      break;
+
   }
+
+  game();
+
 }
 
 /**
@@ -55,44 +63,72 @@ function getComputerChoice() {
 }
 
 /**
- * playRound function takes input from playerChoice
- * and computerChoice variable. computerChoice
- * variable is assigned to getComputerChoice function,
- * while playerChoice isnt linked yet but its function
- * is set up.
+ * playRound function takes input from the computerChoice 
+ * variable. computerChoice variable is assigned to 
+ * getComputerChoice function, and then compares to
+ * players choice.
+ * 
+ * only initalizes when the player makes their choice.
  * 
  * runs the inputs through a nested if else statement
  * hooking from the playersChoice first to find the 
- * outcome and modifies text on DOM displaying the
- * result of the round. 
+ * outcome then tallys the win score to the respictive
+ * plays counter.
  */
 
-function playRound(playerChoice, computerChoice) {
+function playRound(computerChoice) {
   if (playerChoice == "Rock") {
     if (computerChoice == "Rock") {
       roundResult.innerText = "Tie!";
     } else if (computerChoice == "Paper") {
       roundResult.innerText = "You lose! Paper beats Rock!";
+      computerWins += 1;
     } else if (computerChoice == "Scissors") {
       roundResult.innerText = "You win! Rock beats Scissors!"
+      playerWins += 1;
     }
 
   } else if (playerChoice == "Paper") {
     if (computerChoice == "Rock") {
       roundResult.innerText = "You win! Paper beats Rock!";
+      playerWins += 1;
     } else if (computerChoice == "Paper") {
       roundResult.innerText = "Tie!";
     } else if (computerChoice == "Scissors") {
       roundResult.innerText = "You Lose! Scissors beats Paper!"
+      computerWins += 1;
     }
 
   } else if (playerChoice == "Scissors") {
     if (computerChoice == "Rock") {
       roundResult.innerText = "You Lose! Rock beats Scissors!";
+      computerWins+= 1;
     } else if (computerChoice == "Paper") {
       roundResult.innerText = "You win! Scissors beats Paper!";
+      playerWins += 1;
     } else if (computerChoice == "Scissors") {
       roundResult.innerText = "Tie!"
     }
   }
 }   
+
+/**
+ * Function game() runs the playRound() function, then
+ * updates the score counter on the DOM, and always checks
+ * to see what the score is while updating text on the dom
+ * to let the user know what the status of the game is.
+ */
+
+function game() {
+  playRound(computerChoice);
+  playerScore.innerText = playerWins;
+  computerScore.innerText = computerWins;
+
+  if (playerWins == 5 && computerWins < 5) {
+    gameResult.innerText = "You Win!";
+  } else if (playerWins < 5 && computerWins == 5) {
+    gameResult.innerText = "You Lose... :-("
+  } else {
+    gameResult.innerText = "You got this!"
+  }
+}
